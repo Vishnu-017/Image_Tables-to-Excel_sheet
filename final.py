68183a5ab73c6c2a39fc0134a4bf2f23bb5c4c92
Vishnu-017 <107290759+Vishnu-017@users.noreply.github.com>
2023-03-06 18:47:58 +0530

    Update final.py

--- final.py
+++ final.py
@@ -1,20 +1,3 @@
-'''import cv2
-import layoutparser as lp
-image = cv2.imread("bd2.png")
-image = image[..., ::-1]
-
-# load model
-model = lp.PaddleDetectionLayoutModel(config_path="lp://PubLayNet/ppyolov2_r50vd_dcn_365e_publaynet/config",
-                                threshold=0.5,
-                                label_map={0: "Text", 1: "Title", 2: "List", 3:"Table", 4:"Figure"},
-                                enforce_cpu=False,
-                                enable_mkldnn=True)
-# detect
-layout = model.detect(image)
-
-# show result
-show_img = lp.draw_box(image, layout, box_width=3, show_element_type=True)
-show_img.show()'''
 
 from paddleocr import PaddleOCR, draw_ocr
 import cv2
